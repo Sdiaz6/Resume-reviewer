0d2cbebbc9f14903000fe7463c1cdc86979d5dff
Sdiaz6 <145275204+Sdiaz6@users.noreply.github.com>
2025-09-29 16:23:44 -0400

    Create About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <section>
+        <h2 className="section-title">Meet the Team</h2>
+        <div className="card-custom">
+          <h3>Michael</h3>
+          <p>AI Specialist & Resume Specialist</p>
+        </div>
+        <div className="card-custom">
+          <h3>Gillian Dodge</h3>
+          <p>Lead Developer</p>
+        </div>
+        <div className="card-custom">
+          <h3>Silvana Diaz</h3>
+          <p>UX Designer</p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default About;
